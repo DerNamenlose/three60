d20be0c145271e76dcbe313b6eba94a0f11df647
Markus Brueckner <namenlos@geekbetrieb.de>
2024-12-24 13:01:10 +0100

    Formatted error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import WarningIcon from '$lib/components/icons/WarningIcon.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar title="Error" />
+
+<div class="flex h-screen items-center justify-center">
+	<p class="mw-10"><WarningIcon /> {page.error?.message}</p>
+</div>
